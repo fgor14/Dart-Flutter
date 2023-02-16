7750b839ad448a65c0e87d8a2384d70b2f7cb283
ilvostrofighissimousername <lavostramail@gmail.com>
2023-02-16 15:23:09 +0100

    .

--- lotr_cli/bin/lotr_cli.dart
+++ lotr_cli/bin/lotr_cli.dart
@@ -1,5 +1,31 @@
-import 'package:lotr_cli/lotr_cli.dart' as lotr_cli;
+//import 'package:lotr_cli/lotr_cli.dart' as lotr_cli;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lotr_cli.calculate()}!');
+//void main(List<String> arguments) {
+//  print('Hello world: ${lotr_cli.calculate()}!');
+//}
+
+void main(List<String> arguments) 
+{
+  
+  List<int> calc() {
+
+    int count = 5;
+    int first = 0;
+    int second = 1;
+    int next;
+    List<int> fibonacci = [];
+
+  print("The first $count numbers of the Fibonacci sequence are:");
+  for (int i = 0; i < count; i++) {
+    if (i <= 1) {
+      next = i;
+    } else {
+      next = first + second;
+      first = second;
+      second = next;
+    }
+    fibonacci.add(next);
+  }
+  print(fibonacci);
+  
 }
--- lotr_cli/lib/lotr_cli.dart
+++ lotr_cli/lib/lotr_cli.dart
@@ -1,3 +1,28 @@
-int calculate() {
-  return 6 * 7;
+class Fibonacci {
+  int count;
+
+  Fibonacci({
+    required this.count
+  })
+
+  List<int> calc() {
+
+    int count = 5;
+    int first = 0;
+    int second = 1;
+    int next;
+    List<int> fibonacci = [];
+
+  print("The first $count numbers of the Fibonacci sequence are:");
+  for (int i = 0; i < count; i++) {
+    if (i <= 1) {
+      next = i;
+    } else {
+      next = first + second;
+      first = second;
+      second = next;
+    }
+    fibonacci.add(next);
+  }
+  print(fibonacci);
 }
