edcd8a0322b26165c7ec5d9e770b77b13ef47e68
ilvostrofighissimousername <lavostramail@gmail.com>
2023-02-16 14:48:21 +0100

    initial commit

---
+++ lotr_cli/bin/lotr_cli.dart
@@ -0,0 +1,5 @@
+import 'package:lotr_cli/lotr_cli.dart' as lotr_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lotr_cli.calculate()}!');
+}
---
+++ miodart/mioprogetto/bin/mioprogetto.dart
@@ -0,0 +1,5 @@
+import 'package:mioprogetto/mioprogetto.dart' as mioprogetto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mioprogetto.calculate()}!');
+}
